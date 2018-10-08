3855d41abc9d70f87403d5f7f40b575c841ed4e2
Dylano Hartman <dylano.hartman@gmail.com>
2018-10-08 23:44:02 +0200

    14

--- routes/api/users.js
+++ routes/api/users.js
@@ -33,7 +33,8 @@ router.post('/register', (req, res) => {
   User.findOne({email: req.body.email})
   .then(user => {
     if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
+      errors.email = 'Email already exists'
+      return res.status(400).json(errors);
     }
     else {
 
